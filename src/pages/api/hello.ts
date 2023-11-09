4fce7b46c5fddead199a1c1ebd26f0430e872670
Zadson <zadson.silva@aluno.senai.br>
2023-11-08 21:16:16 -0300

    Alterado

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
+type Data = Array<{
   name: string
-}
+  telefone: string
+  email: string
+}>;
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'Zady!!!' })
+  res.status(200).json([
+    {name: 'Zadson', telefone: '123456789', email: 'zadson65@gmail.com'},
+    {name: 'Welll', telefone: '123456789', email: 'Well@gmail.com'},
+    {name: 'Higor', telefone: '123456789', email: 'Higor@gmail.com'},
+    {name: 'Bruno', telefone: '123456789', email: 'Bruno@gmail.com'}
+  ]);
 }
